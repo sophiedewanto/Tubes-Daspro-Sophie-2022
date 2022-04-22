5ce893245e114a08e261fb26719f4a9628873ee0
sophiedewanto <103799730+sophiedewanto@users.noreply.github.com>
2022-04-22 21:25:47 +0700

    Delete B02.py

--- B02.py
+++
@@ -1,9 +0,0 @@
-import time
-
-pertanyaan = input("Apa pertanyaanmu? ")
-
-time = int(time.time()*1000)
-
-jawaban = ["Ya","Tidak mungkin","Bisa jadi","Coba tanya lagi","Tidak","Tentunya","Mungkin","Kurasa tidak","Harus","Mungkin suatu hari"]
-
-print(jawaban[time%10])
